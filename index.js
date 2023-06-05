f11e4bc40b8816f17aed0bcb1125244fff094194
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-06-05 21:23:09 +0100

    Created separate questions.js file

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const generateMarkdown = require('./utils/generateMarkdown')
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
