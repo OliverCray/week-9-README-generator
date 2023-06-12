35a7f4a9d2373294094221bc7cd23497db102bb5
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-06-12 13:29:19 +0100

    Added all required questions to questions array

--- utils/questions.js
+++ utils/questions.js
@@ -1,3 +1,56 @@
-const questions = []
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description for your project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions on how to use your project'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Add credits for collaborators and any third-party assets'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ['APACHE 2.0', 'GPL 3.0', 'MIT', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can other users contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How would another user test your project?',
+        default: 'npm test'
+    },
+]
 
 module.exports = questions
