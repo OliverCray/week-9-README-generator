a93fb74cdbea28cfd27ecd96334927c113a44c51
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-06-12 12:50:21 +0100

    Added require statement for questions.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const generateMarkdown = require('./utils/generateMarkdown')
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
+const questions = require('./utils/questions')
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
